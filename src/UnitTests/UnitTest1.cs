a58d7e4b44b987079fe547d38c6516388c4b55b6
Ihor Susoi <50860529+IhorSusoi@users.noreply.github.com>
2025-03-17 22:39:13 +0200

    test github actions

--- src/UnitTests/UnitTest1.cs
+++ src/UnitTests/UnitTest1.cs
@@ -14,7 +14,7 @@ namespace UnitTests
         public void Test1()
         {
             var class1 = new Class1();
-            class1.ShouldNotBeNull();
+            class1.ShouldBeNull();
         }
     }
 }
